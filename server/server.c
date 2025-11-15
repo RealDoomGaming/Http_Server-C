e73ea1944efa7f2adff603d563bf7055629454d7
Hauerm <hauerm@edu.htl-villach.at>
2025-11-15 20:47:26 +0100

    Added the final touches -> Connections can be built and closed, Server can send something to the client, Should also be multithreaded now

--- server/server.c
+++ server/server.c
@@ -1,7 +1,13 @@
+#include <bits/pthreadtypes.h>
 #include <netinet/in.h>
+#include <pthread.h>
+#include <stddef.h>
 #include <stdio.h>
+#include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <threads.h>
+#include <unistd.h>
 
 #define PORT 8080
 #define BACKLOG 10
@@ -72,10 +78,88 @@ int server_listen(server_t *server) {
     return err;
   }
 
-  return 0;
+  return err;
 }
 
-int server_accept(server_t *server) {}
+int server_accept(server_t *server) {
+  // err variable and the connection file descripton
+  int err = 0;
+  int conn_fd;
+  // also make the client_len for the accept later
+  socklen_t client_len;
+  // then make a struct the same with the server but just for the client
+  struct sockaddr_in client_addr;
+
+  // actually get the size of the client_addr struct we made
+  client_len = sizeof(client_addr);
+
+  // then we try to call accept with the listen_fd, and then we also give accept
+  // our client_addr but cast it to the sockaddr struct lastly we give the
+  // accept function the client length
+  err = (conn_fd = accept(server->listen_fd, (struct sockaddr *)&client_addr,
+                          &client_len));
+
+  // if we had an error then we return and just print and give error back
+  if (err == -1) {
+    perror("accept");
+    printf("failed accepting connection\n");
+    return err;
+  }
+
+  // else we print Client Connected bc we connected succesfully
+  printf("Client connected!\n");
+
+  // now we need to send something with write
+  // in the content needs to be the protocoll, the status code
+  // the content type and then after that the actual text or whatever you want
+  // to send
+  char *content = "HTTP/1.0 200 OK\r\n"
+                  "Content-Type: text/html\n"
+                  "\r\n"
+                  "Ingo Bingo";
+  // size of our content
+  size_t contentSize = strlen(content);
+
+  // then we write it to the client with the client fd and the content and the
+  // size of the content
+  err = write(conn_fd, content, contentSize);
+
+  // if error then we do the usual stuff
+  if (err == -1) {
+    perror("write");
+    printf("failed to write to client\n");
+    return err;
+  }
+
+  // then we close the connection with conn_fd and if any error happened then
+  // do error stuff
+  err = close(conn_fd);
+  if (err == -1) {
+    perror("close");
+    printf("failed to close connection\n");
+    return err;
+  }
+
+  // For testing purposes we print this
+  printf("Client Connection Close\n");
+
+  return err;
+}
+
+void *serverAcceptThreadFunc(void *arg) {
+  // comments for everything here is in the main function but commented out
+  server_t *server = (server_t *)arg;
+  int err = 0;
+
+  for (;;) {
+    err = server_accept(server);
+
+    if (err) {
+      printf("Failed accepting connection\n");
+      pthread_exit(NULL);
+    }
+  }
+}
 
 int main() {
   // main method
@@ -90,17 +174,32 @@ int main() {
     return err;
   }
 
+  // for better perfromance when multiple clients connect we use multiple
+  // threads Create Thread T for later here:
+  pthread_t threadAccept;
+  // here we actually create the thread and it runs automatically:
+  // first we give it our thread t we created before
+  // then we give it NULL because we have no Thread attributes to give
+  // then we give it our function and lastly we give it the parameter for the
+  // function
+  pthread_create(&threadAccept, NULL, serverAcceptThreadFunc, &server);
+  // now we wait for the thread to finish (it will never finish except when it
+  // closes)
+  pthread_join(threadAccept, NULL);
+
+  /*
   // if no error then enter and infinite loop
   for (;;) {
     // where we try to accept incoming connections
     err = server_accept(&server);
     // else if there is an error while accepting we return out of the inifite
     // loop and the programm closes
-    if (err) {
+    if (err != 0) {
       printf("Failed accepting connection\n");
       return err;
     }
   }
+  */
 
   return 0;
 }
