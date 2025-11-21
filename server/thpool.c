a737949a9b49f57e32060b8e34421a5dfba0fb5e
Hauerm <hauerm@edu.htl-villach.at>
2025-11-21 15:31:40 +0100

    Added more struct to the thpool.c file

--- server/thpool.c
+++ server/thpool.c
@@ -25,7 +25,7 @@ typedef struct bsem {
   pthread_cond_t cond; // conditional variable which lets a thread go eep while
                        // waiting for the semaphore to become available again
                        // and wakes it  up once someone signals it
-  int v; // actual value, can only be 0 and 1
+  int v;               // actual value, can only be 0 and 1
 } bsem;
 
 // firstly the struct for our job
@@ -38,9 +38,28 @@ typedef struct job {
 // secondly our job queue struct
 typedef struct jobqueue {
   pthread_mutex_t rwmutex; // this is for the read and write access of our jobs
-  job *front;     // then we have a pointer to the first object in the queue
-  job *rear;      // also a pointer to the second object in the queue
-  bsem *has_jobs; // bsem is shorthand for binary semaphore, explained when we
-                  // define it
-  int len;        // finally the number of jobs in the queue
+  job *front;    // then we have a pointer to the first object in the queue
+  job *rear;     // also a pointer to the second object in the queue
+  bsem *hasJobs; // bsem is shorthand for binary semaphore, explained when we
+                 // define it
+  int len;       // finally the number of jobs in the queue
 } jobqueue;
+
+// now the thread struct we will use
+// We use a struct and not just a thread because we can contoll it better and //
+// access it via and id
+typedef struct thread {
+  int id;                   // the id for our thread
+  pthread_t pthread;        // our actual thread
+  struct thpool_ *thpool_p; // the thread pool our thread belongs too
+} thread;
+
+// our thread pool for the thread struct later
+typedef struct thpool_ {
+  thread **threads; // a pointer to the threads
+  volatile int numThreadsAlive;
+  volatile int numThreadsWorking;
+  pthread_mutex_t thcountLock;
+  pthread_cond_t threadAllIdle;
+  jobqueue jobqueue;
+} thpool_;
