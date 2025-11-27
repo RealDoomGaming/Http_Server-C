e6ad80040ea7d6e5503577cb9d3f13e96e150d8c
Hauerm <hauerm@edu.htl-villach.at>
2025-11-27 19:03:59 +0100

    Started working on the actuall thread pool now and I finished my threadpool header file

---
+++ Thpool/thpool.h
@@ -0,0 +1,32 @@
+#ifndef THPOOL
+#define THPOOL
+
+#include "../DataStructures/queue.h"
+#include <pthread.h>
+
+typedef struct ThreadPool {
+  // struct for the thread pool with the number of threads, then if its active,
+  // then the queue where the jobs go then the actual array of threads then the
+  // mutex which basically acts like a lock for the threads and the condition is
+  // so we can reduce the impact of our threads on the cpu, the signal just
+  // allows the thread to remain idle until it is signaled to continue
+  int numThreads;
+  int active;
+  queue threadQueue;
+  pthread_t *pool;
+  pthread_mutex_t mutex;
+  pthread_cond_t signal;
+} threadPool;
+
+typedef struct ThreadJob {
+  // for this struct we need a void pointer function with a void pointer return
+  // and a void pointer arg and then we also need the argument itself in this
+  // struct which ofc is going to be a void pointer arg
+  void *(*job)(void *);
+  void *arg;
+} threadJob;
+
+threadPool threadPoolInit(int numThreads);
+threadJob threadJobInit(void *(*job)(void *), void *arg);
+
+#endif // !THPOOL
